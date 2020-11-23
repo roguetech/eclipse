d62ac21cc8e2858478312f7dc6aa7264effc1e33
Patrick Roughan <padraig.oruchain@gmail.com>
2020-11-23 08:26:37 +0000

    add files

---
+++ week7/src/week7/test.java
@@ -0,0 +1,13 @@
+package week7;
+
+public class test {
+
+	public test() {
+		System.out.println("test class");
+	}
+
+	public static void main(String[] args) {
+		test t = new test();
+	}
+
+}
---
+++ week8/src/week8/week8.java
@@ -0,0 +1,7 @@
+package week8;
+
+public class week8 {
+	public static void main(String[] arg) {
+		System.out.println("test 1");
+	}
+}
